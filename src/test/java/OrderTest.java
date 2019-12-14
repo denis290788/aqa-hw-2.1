1208e533db2ef83b583d9fea7cbc9ced2d199f9e
denis290788 <denis290788@gmail.com>
2019-12-14 16:19:16 +0300

    commit 2

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -10,11 +13,11 @@ public class OrderTest {
     @Test
     void shouldConfirmOrder() {
         open("http://localhost:9999");
-        SelenideElement form = $("form form_size_m form_theme_alfa-on-white");
-        form.$("[data-test-id=\"name\"] input").setValue("Василий Петров");
-        form.$("[data-test-id=\"phone\"] input").setValue("+79118676755");
-        form.$("[data-test-id=\"agreement\"]").click();
-        form.$("button button_view_extra button_size_m button_theme_alfa-on-white").click();
-        form.$(".order-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        //SelenideElement form = $("form form_size_m form_theme_alfa-on-white");
+        $("[data-test-id=\"name\"] input").setValue("Василий Петров");
+        $("[data-test-id=\"phone\"] input").setValue("+79118676755");
+        $("[data-test-id=\"agreement\"]").click();
+        $(byText("Продолжить")).click();
+        $(byText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")).shouldBe(visible);
     }
 }
